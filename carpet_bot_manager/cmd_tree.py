bcccbbca261089becb05aa301ba08209a3afb828
YehowahLiu <redme@live.cn>
2021-08-20 20:07:48 +0800

    Bump version to 0.2.1

--- carpet_bot_manager/cmd_tree.py
+++ carpet_bot_manager/cmd_tree.py
@@ -12,18 +12,17 @@ from carpet_bot_manager.messages import generate_multiline_help, tr
 def register_command(server: PluginServerInterface, manager: BotManager):
     meta = server.get_self_metadata()
     general_help = generate_multiline_help(tr(
-                'help.general', name=meta.name,
-                version=meta.version, prefix=constants.Prefix,
-                prefix2=constants.Prefix2
-            ))
+        'help.general', name=meta.name,
+        version=meta.version, prefix=constants.Prefix,
+        prefix2=constants.Prefix2))
 
     action_help = generate_multiline_help(tr('help.action', prefix=constants.Prefix, prefix2=constants.Prefix2))
 
-    existed_bot_node = Text('bot').requires(  # suggests 和 requires 共用时表现异常
-        lambda src, ctx: manager.check_list(ctx['bot']),
-        lambda src: tr('command.unknown_bot')
-    ).suggests(lambda: manager.bots_in_list)
-
+    def get_existed_bot_node():
+        return Text('bot').requires(  # suggests 和 requires 共用时表现异常
+            lambda src, ctx: manager.check_list(ctx['bot']),
+            lambda src: tr('command.unknown_bot')
+        ).suggests(lambda: manager.bots_in_list)
 
     def bot_name_required_node(cmd: Union[str, set]):
         return Literal(cmd).runs(lambda src: src.reply(tr('command.bot_name_required')))
@@ -61,23 +60,23 @@ def register_command(server: PluginServerInterface, manager: BotManager):
             Integer('page').runs(lambda src, ctx: manager.list_bots(src, ctx['page'])))
     ).then(
         bot_name_required_node('spawn').then(
-            existed_bot_node.runs(lambda src, ctx: manager.spawn_bot(src, ctx['bot'])))
+            get_existed_bot_node().runs(lambda src, ctx: manager.spawn_bot(src, ctx['bot'])))
     ).then(
         bot_name_required_node('kill').then(
-            existed_bot_node.runs(lambda src, ctx: manager.kill_bot(src, ctx['bot'])))
+            get_existed_bot_node().runs(lambda src, ctx: manager.kill_bot(src, ctx['bot'])))
     ).then(
         Literal('action').runs(lambda src: src.reply(action_help)).then(
-            existed_bot_node.runs(lambda src, ctx: manager.list_action(src, ctx['bot'])).then(
+            get_existed_bot_node().runs(lambda src, ctx: manager.list_action(src, ctx['bot'])).then(
                 Literal('clear').runs(lambda src, ctx: manager.clear_action(src, ctx['bot']))
             ).then(
                 Literal('exec').runs(lambda src, ctx: manager.exec_action(src, ctx['bot']))))
     ).then(
         bot_name_required_node('del').then(
-            existed_bot_node.runs(lambda src, ctx: manager.del_bot(src, ctx['bot']))
+            get_existed_bot_node().runs(lambda src, ctx: manager.del_bot(src, ctx['bot']))
         )
     ).then(
         bot_name_required_node({'info', 'desc'}).then(
-            existed_bot_node.runs(lambda src, ctx: manager.info_bots(src, ctx['bot'])).then(
+            get_existed_bot_node().runs(lambda src, ctx: manager.info_bots(src, ctx['bot'])).then(
                 GreedyText('desc').runs(
                     lambda src, ctx: manager.set_desc(src, ctx['bot'], ctx['desc']))
             )
@@ -86,7 +85,9 @@ def register_command(server: PluginServerInterface, manager: BotManager):
 
     if manager.config.debug:
         node = node.then(
-            Literal('debug').runs(lambda src: src.reply(manager.bots_in_list))
+            Literal('debug').runs(lambda src: src.reply(manager.bots_in_list)).then(
+                Literal('bots_in_list').runs(lambda src: src.reply(manager.bots_in_list))
+            )
         )
 
     server.register_command(node)
